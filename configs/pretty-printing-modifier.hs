c95b0df7661356d8d7ce57b08a1343f620eeff65
ivanbrennan <ivan.brennan@gmail.com>
2020-09-15 12:01:10 -0400

    test statusBar' with workspaceNamesPP
    After rebuilding, I ran bin/run-in-xephyr.sh and renamed a workspace,
    then confirmed everything looked and behaved as expected -- i.e.
    renaming workspace "1" to "foo" showed "1:foo" in its position on
    xmobar, and switching workspaces updated xmobar appropriately.

--- configs/pretty-printing-modifier.hs
+++ configs/pretty-printing-modifier.hs
@@ -1,18 +1,21 @@
 import XMonad
 import qualified XMonad.StackSet as W
+import XMonad.Actions.WorkspaceNames (renameWorkspace, workspaceNamesPP)
 import XMonad.Hooks.DynamicLog (statusBar', xmobarPP)
 import qualified Data.Map as M
 
 main :: IO ()
 main = xmonad =<< statusBar' "xmobar" xpp toggleStrutsKey conf
   where
-    xpp = return xmobarPP
+    xpp = workspaceNamesPP xmobarPP
 
     toggleStrutsKey = const (mod4Mask, xK_b)
 
     conf = def
       { keys = const . M.fromList $
-          [ ((mod4Mask, xK_o), spawn (terminal def)) ]
+          [ ((mod4Mask, xK_o), spawn (terminal def)),
+            ((mod4Mask, xK_equal), renameWorkspace def)
+          ]
           ++
           [ ((mod4Mask, k), windows $ W.greedyView i)
             | (i, k) <- zip (workspaces def) [xK_1 .. xK_9]
