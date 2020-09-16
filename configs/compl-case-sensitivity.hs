55ebb0e57a22a8b148668bb35de3a85b5431b082
ivanbrennan <ivan.brennan@gmail.com>
2020-09-15 23:10:11 -0400

    test DynamicProjects case insensitive prompt

--- configs/compl-case-sensitivity.hs
+++ configs/compl-case-sensitivity.hs
@@ -1,15 +1,14 @@
 import XMonad
-import XMonad.Layout.WorkspaceDir (changeDir', workspaceDir)
+import XMonad.Actions.DynamicProjects (dynamicProjects, changeProjectDirPrompt')
 import XMonad.Prompt (ComplCaseSensitivity (ComplCaseSensitive))
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
 
 main :: IO ()
-main = xmonad def
-  { layoutHook = workspaceDir "~" (layoutHook def),
-    keys = const . M.fromList $
+main = xmonad $ dynamicProjects [] def
+  { keys = const . M.fromList $
       [ ((mod4Mask, xK_o), spawn (terminal def)),
-        ((mod4Mask, xK_equal), changeDir' (ComplCaseSensitive False) def)
+        ((mod4Mask, xK_equal), changeProjectDirPrompt' (ComplCaseSensitive False) def)
       ]
       ++
       [ ((mod4Mask, k), windows $ W.greedyView i)
